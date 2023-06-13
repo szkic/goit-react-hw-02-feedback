a84f93bc331caed64127f814557eabd5279dca11
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-06-13 18:30:18 +0200

    Task 4

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -6,10 +6,15 @@ export const Statistics = ({
   positivePercentage,
 }) => (
   <>
-    <p>Good: {good}</p>
-    <p>Neutral: {neutral}</p>
-    <p>Bad: {bad}</p>
-    <p>Total: {total}</p>
-    <p>Positive feedback: {positivePercentage}</p>
+    {total === 0 && <h4>No feedback given</h4>}
+    {total > 0 && (
+      <>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Positive feedback: {positivePercentage}</p>
+      </>
+    )}
   </>
 );
